a555af55af50b2c22a7a92f4ef5b35e5496a5310
Jameson Nash <vtjnash@gmail.com>
2013-03-20 14:22:03 -0400

    pset3 demo3

--- pset3/convergence.jl
+++ pset3/convergence.jl
@@ -11,24 +11,56 @@ Kn(n::Integer, x::FloatingPoint, y::FloatingPoint) = exp((-x^2-y^2)/2)*sqrt(n-1)
 #sine_kernel(m, x, y) = 2*sqrt(pi)*m*sin(x-y)/(x-y)
 sine_kernel(m, x, y) = sqrt(2*m-1)/sqrt(m)/pi*sin(2*sqrt(int(m))*(x-y))/(x-y)
 
-x = 2.005
-ys = -10:.1:10
-ms = Integer[1, 2, 4, 6, 9, BigInt(12), BigInt(15), BigInt(18), BigInt(21)]
-filename = "converg" #nothing
-for m in ms
-    p = plot(ys, [sine_kernel(m, x, y) for y in ys], "b--",
-         ys, [Kn(2m, x, y) for y in ys], "r-")
-    l1,l2 = p.content1.components
-    setattr(l1, "label", "Sine Kernel")
-    setattr(l2, "label", "K_n")
-    add(p, Legend(0.1,0.9, Any[l1,l2]))
-    setattr(p, "title", "K_n vs. Sine Kernel at X=$x - M=$m")
-    setattr(p, "xlabel", "Y")
-    setattr(p, "ylabel", "Z")
-    Winston.display(p)
-    if isa(filename,String)
-        file(p, "$(filename)_$(m).png")
+demo2() = demo2(nothing)
+function demo2(filename::Union(Nothing,String))
+    x = 2.005
+    ys = -10:.1:10
+    ms = Integer[1, 2, 4, 6, 9, BigInt(12), BigInt(15), BigInt(18), BigInt(21)]
+    for m in ms
+        p = plot(ys, [sine_kernel(m, x, y) for y in ys], "b--",
+             ys, [Kn(2m, x, y) for y in ys], "r-")
+        l1,l2 = p.content1.components
+        setattr(l1, "label", "Sine Kernel")
+        setattr(l2, "label", "K_n")
+        add(p, Legend(0.1,0.9, Any[l1,l2]))
+        setattr(p, "title", "K_n vs. Sine Kernel at X=$x - M=$m")
+        setattr(p, "xlabel", "Y")
+        setattr(p, "ylabel", "Z")
+        Winston.display(p)
+        if isa(filename,String)
+            file(p, "$(filename)_$(m).png")
+        end
+        pause()
     end
-    pause()
 end
+
+demo3() = demo3(nothing)
+function demo3(filename::Union(Nothing,String))
+    x = 0.005
+    ys = -30:.1:10
+    ns = Integer[1, 2, 3, 4, 5, 10, 15, 20, 40, 60]
+    for n in ns
+        p = plot(ys, [(airy(x)*airyprime(y)-airyprime(x)*airy(y))/(x-y) for y in ys], "b--",
+             ys, [1/sqrt(2)/n^(1/6)*Kn(n>20?BigInt(n):n, sqrt(2n)+x/sqrt(2)/n^(1/6), sqrt(2n)+y/sqrt(2)/n^(1/6)) for y in ys], "r-")
+        l1,l2 = p.content1.components
+        setattr(l1, "label", "Theoretical Limit")
+        setattr(l2, "label", "Airy Process")
+        add(p, Legend(0.1,0.9, Any[l1,l2]))
+        setattr(p, "title", "Airy Process Convergence at X=$x - N=$n")
+        setattr(p, "xlabel", "Y")
+        setattr(p, "ylabel", "Z")
+        Winston.display(p)
+        if isa(filename,String)
+            file(p, "$(filename)_$(n).png")
+        end
+        pause()
+    end
+
+end
+
+function demo()
+    demo2()
+    demo3()
+end
+
 end
